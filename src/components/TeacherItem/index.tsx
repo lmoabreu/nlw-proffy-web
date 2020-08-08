7d0d0fce42c9c259f5bd7ed994daea651c5e555d
Lucas Magno <lmoabreu@gmail.com>
2020-08-07 21:29:52 -0300

    habilitar link para whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
     id: number;
@@ -20,6 +21,11 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher}) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        });
+    }
     return (
         <article className="teacher-item">
             <header>
@@ -35,10 +41,10 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher}) => {
                     Preço/Hora
                     <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} target="_blank">
                     <img src={whatsappIcon} alt="Whatsapp" />
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
